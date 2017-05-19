548d99486543f4d97afe6f332916fe47b42654b0
Dqicool <dqicool@outlook.com>
2017-05-19 15:23:58 +0800

    参数调整

--- ModelP.m
+++ ModelP.m
@@ -1,12 +1,14 @@
-function PauseTotal = ModelP(TempPool, PlayAvgSpeed, CodeSpeed)
+function [PauseTotal, PauseCount] = ModelP(TempPool, PlayAvgSpeed, CodeSpeed)
     DataSize    = max(size(CodeSpeed));
     time        = 0;
     PauseTotal  = zeros(DataSize,1);
     StartSymbol = true(DataSize,1);
+    PauseCount  = zeros(DataSize,1);
     while time < 30000
         time        = time + 1;
-        TempPool    = TempPool - StartSymbol .* CodeSpeed + PlayAvgSpeed;
-        StartSymbol = StartSymbol - (TempPool < 1) .* StartSymbol + ...     %刚刚开始卡顿的数目
+        TempPool    = TempPool - StartSymbol .* CodeSpeed + PlayAvgSpeed  ./ 2.128154422489637;
+        PauseCount  = PauseCount + (TempPool < CodeSpeed) .* StartSymbol;
+        StartSymbol = StartSymbol - (TempPool < CodeSpeed) .* StartSymbol + ...     %刚刚开始卡顿的数目
                       (~StartSymbol) .* (TempPool > 2700 * CodeSpeed);      %卡顿还没有开始的数目
         PauseTotal  = PauseTotal + (~StartSymbol);
     end
--- Modeling.m
+++ Modeling.m
@@ -1,6 +1,6 @@
-function [InitialDataAmong ,PauseTotal, InitialDelay] = Modeling(E2ERTT, PlayAvgSpeed, InitialSpeedPeak, CodeSpeed)
+function [InitialDataAmong ,PauseTotal, InitialDelay, PauseCount] = Modeling(E2ERTT, PlayAvgSpeed, InitialSpeedPeak, CodeSpeed)
 % The discribetion is in describe.m
 %矩阵化编程
-    [InitialDataAmong ,InitialDelay, TempPool] = ModelB1I(E2ERTT, InitialSpeedPeak, CodeSpeed, PlayAvgSpeed);
-    PauseTotal = ModelB1P(TempPool, PlayAvgSpeed, CodeSpeed);
+    [InitialDataAmong ,InitialDelay, TempPool]  = ModelI(E2ERTT, InitialSpeedPeak, CodeSpeed, PlayAvgSpeed);
+    [PauseTotal, PauseCount]                    = ModelP(TempPool, PlayAvgSpeed, CodeSpeed);
 end
