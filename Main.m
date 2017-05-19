2359ddf8f2e8ab843c6fa1c56e4879e1d93d7486
Dqicool <dqicool@outlook.com>
2017-05-19 10:21:17 +0800

    Create Main.m

---
+++ Main.m
@@ -0,0 +1,26 @@
+cc
+load data
+OOInitialDataAmong  = zeros(1, max(size(CodeSpeed)));
+OOInitialDelay      = zeros(1, max(size(CodeSpeed)));
+OOPauseTotal        = zeros(1, max(size(CodeSpeed)));
+OOPauseCount        = zeros(1, max(size(CodeSpeed)));
+tic
+    for i = 1:max(size(CodeSpeed))
+        [OOInitialDataAmong(i), OOPauseTotal(i), OOInitialDelay(i), OOPauseCount(i)] = ...
+        ModelB2(E2ERTT(i), PlayAvgSpeed(i), InitialSpeedPeak(i), CodeSpeed(i));
+    end
+toc
+clear i;
+OOInitialDelay      = OOInitialDelay';
+OOPauseTotal        = OOPauseTotal';
+OOInitialDataAmong  = OOInitialDataAmong';
+OOPauseCount        = OOPauseCount';
+
+
+FigurePlot(PlayAvgSpeed, PauseTotal, OOPauseTotal, InitialSpeedPeak, InitialDelay, OOInitialDelay, InitialDataAmong, OOInitialDataAmong, CodeSpeed, PauseCount, OOPauseCount)
+%作图
+[ErrPC, ErrID,ErrPT,ErrIDA] = ...
+ErrorAnalyse(PauseCount ,InitialDelay, PauseTotal, InitialDataAmong, OOInitialDelay, OOPauseTotal, OOInitialDataAmong, OOPauseCount)
+%误差分析
+
+
