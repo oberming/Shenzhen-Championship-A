f3b259ff457b8a72741958774f98d27a0b8f8f54
Dqicool <dqicool@outlook.com>
2017-05-27 19:38:04 +0800

    recover

--- ModelP.m
+++ ModelP.m
@@ -6,7 +6,7 @@ function [PauseTotal, PauseCount] = ModelP(TempPool, PlayAvgSpeed, CodeSpeed)
     PauseCount  = zeros(DataSize,1);
     while time < 30000
         time        = time + 1;
-        TempPool    = TempPool - StartSymbol .* CodeSpeed + PlayAvgSpeed  ./ 2.128154422489637;
+        TempPool    = TempPool - StartSymbol .* CodeSpeed + PlayAvgSpeed;
         PauseCount  = PauseCount + (TempPool < CodeSpeed) .* StartSymbol;
         StartSymbol = StartSymbol - (TempPool < CodeSpeed) .* StartSymbol + ...     %刚刚开始卡顿的数目
                       (~StartSymbol) .* (TempPool > 2700 * CodeSpeed);      %卡顿还没有开始的数目
