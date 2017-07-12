03d03bf39201d2091848cf534bd257f0e6cd85d4
oberming <409754011@qq.com>
2017-07-12 14:57:50 +0800

    preinitialdelay model

--- Modeling.m
+++ Modeling.m
@@ -3,25 +3,29 @@ function [InitialDataAmong ,PauseTotal, InitialDelay, PauseCount] = Modeling(E2E
 % 引入 Download Temp Pool
     global DataSize
     DataSize        = max(size(CodeSpeed));
+    %global PreID
+    [PreID]         = preid(E2ERTT, PlayAvgSpeed, InitialSpeedPeak, CodeSpeed);
     [InitialDataAmong ,InitialDelay, DownloadTempPool]  = ModelI(E2ERTT, InitialSpeedPeak, CodeSpeed, PlayAvgSpeed);
+    InitialDelay = InitialDelay + PreID;
     [PauseTotal, PauseCount]                            = ModelP(DownloadTempPool, PlayAvgSpeed, CodeSpeed, E2ERTT);
 end
 
 function [InitialDataAmong, InitialDelay, DownloadTempPool] = ModelI(E2ERTT, InitialSpeedPeak, CodeSpeed, PlayAvgSpeed)
     global DataSize    
-    InitialDelay        = (5.5 .* E2ERTT .* ones(DataSize, 1));
-    StartSymbol         = false(DataSize, 1);
-    DownloadTempPool    = (zeros(DataSize, 1));
-    MaxCwnd             = fix(InitialSpeedPeak .* E2ERTT);
+    InitialDelay        = (5.5 .* E2ERTT .* ones(DataSize, 1));     %初始时延基本值为5.5个端到端时间
+    StartSymbol         = false(DataSize, 1);                       %开始播放的判断
+    DownloadTempPool    = (zeros(DataSize, 1));                     %引入下载池
+    MaxCwnd             = fix(InitialSpeedPeak .* E2ERTT);          %fix为向0取整，最大窗口大小为一单位时间内的下载量
     CurrentCwnd         = 1072;                                            %cwnd1 = 2144
     while sum(StartSymbol) < DataSize
-        InitialDelay        = InitialDelay + (~StartSymbol) .* E2ERTT;
+        InitialDelay        = InitialDelay + (~StartSymbol) .* E2ERTT;%每次若没达到开始播放的条件，增加一个单位的时间。
         CurrentCwnd         = 2 * CurrentCwnd .* (CurrentCwnd < 0.5 * MaxCwnd) + ...
-                              0.75 * MaxCwnd .* (CurrentCwnd >= 0.5 * MaxCwnd);
-        CurrentSpeed        = (~StartSymbol) .* CurrentCwnd;
-        DownloadTempPool    = DownloadTempPool + CurrentSpeed;
-        StartSymbol         = logical((DownloadTempPool > CodeSpeed .* 4192)  + ...
-                                      (InitialSpeedPeak < 7000) .* (PlayAvgSpeed < 362) .* (DownloadTempPool > 200 * CodeSpeed));
+                              0.75 * MaxCwnd .* (CurrentCwnd >= 0.5 * MaxCwnd);%瞬时窗口大小的变化
+        CurrentSpeed        = (~StartSymbol) .* CurrentCwnd;            %瞬时速度即为瞬时窗口的大小
+        DownloadTempPool    = DownloadTempPool + CurrentSpeed;          %缓冲池
+        StartSymbol         = logical((DownloadTempPool > CodeSpeed .* 4192)  + ...             %原来是4192
+                                      (InitialSpeedPeak < 7000) .* (PlayAvgSpeed < 362) ... 
+                                      .* (DownloadTempPool > 200 * CodeSpeed));%播放开始的判断条件，即下载池达到4秒或（初始峰速小于7000且播放速率小玉362且下载池大于200毫秒
     end
     InitialDataAmong = DownloadTempPool / 8;
 end
